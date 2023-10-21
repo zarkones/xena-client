8edc806b2e9905414c0543f6cbedf426b3d3cb00
zarkones.xena@gmail.com <zarkones.xena@gmail.com>
2023-10-21 15:33:35 +0200

    bug fixing a bug fix

--- routes.go
+++ routes.go
@@ -1,28 +1,27 @@
 package c2api
 
-import (
-	_ "embed"
-	"strings"
-)
+// import (
+// 	_ "embed"
+// )
 
-//go:embed routes.txt
-var RawRoutesDoc string
+// //go:embed routes.txt
+// var RawRoutesDoc string
 
-const R_FETCH_MESSAGES = 0
-const R_MESSAGE_RESPOND = 1
+// const R_FETCH_MESSAGES = 0
+// const R_MESSAGE_RESPOND = 1
 
-var RouteMap map[int][]string = func() (rMap map[int][]string) {
-	registeredObfuscatedRoutes := []int{
-		R_FETCH_MESSAGES, R_MESSAGE_RESPOND,
-	}
+// var RouteMap map[int][]string = func() (rMap map[int][]string) {
+// 	registeredObfuscatedRoutes := []int{
+// 		R_FETCH_MESSAGES, R_MESSAGE_RESPOND,
+// 	}
 
-	rMap = make(map[int][]string, len(registeredObfuscatedRoutes))
+// 	rMap = make(map[int][]string, len(registeredObfuscatedRoutes))
 
-	wlChunks := chunkSlice(strings.Split(RawRoutesDoc, "\n"), len(registeredObfuscatedRoutes))
+// 	wlChunks := chunkSlice(strings.Split(RawRoutesDoc, "\n"), len(registeredObfuscatedRoutes))
 
-	for chunkIndex, chunk := range wlChunks {
-		rMap[registeredObfuscatedRoutes[chunkIndex]] = chunk
-	}
+// 	for chunkIndex, chunk := range wlChunks {
+// 		rMap[registeredObfuscatedRoutes[chunkIndex]] = chunk
+// 	}
 
-	return rMap
-}()
+// 	return rMap
+// }()
